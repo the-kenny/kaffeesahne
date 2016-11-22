2e9f91a586005313263baf23019e0551de4b8dfd
Moritz Ulrich <moritz@tarn-vedra.de>
2016-11-22 11:42:07 +0100

    shell.nix: Cleanup

--- shell.nix
+++ shell.nix
@@ -11,7 +11,15 @@ let
 in
 stdenv.mkDerivation {
   name = "rust-opengl";
-  buildInputs = with pkgs; [ cargo rustc gdb linuxPackages.perf ];
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    cmake
+    gcc
+    gdb
+    linuxPackages.perf
+    pkgconfig
+  ];
   shellHook = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${libPath}";
 
   RUST_BACKTRACE=1;
